ef7011c8b992607fea6c4d659b46e2716a68d644
Manoj S <142795422+Manoj-S-mnj@users.noreply.github.com>
2024-07-25 22:24:53 +0530

    Update largest subarray sum problem.py

--- largest subarray sum problem.py
+++ largest subarray sum problem.py
@@ -1,15 +1,10 @@
 def largest_subarray_sum(arr):
     if not arr:
         return 0
-    
     current_max = global_max = arr[0]
-    
     for num in arr[1:]:
         current_max = max(num, current_max + num)
         global_max = max(global_max, current_max)
-    
     return global_max
-
-# Test case
 arr = [-2,-3,4,-1,-2,1,5,-3]
 print("The largest subarray sum is:", largest_subarray_sum(arr))
